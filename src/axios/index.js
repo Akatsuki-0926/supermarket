4f55bfe8959691e5d65b5151e2363dae5fb07ed6
Akatsuki_01 <930466213@qq.com>
2021-12-06 16:38:47 +0800

    添加静态资源

--- src/axios/index.js
+++ src/axios/index.js
@@ -3,7 +3,8 @@ import axios from 'axios'
 export function getRequest(config) {
   const http = axios.create({
     baseURL: 'http://localhost:8088',
-    timeout: 5000
+    timeout: 5000,
+    method: 'POST'
   })
 
   return http(config)
